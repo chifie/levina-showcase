156322aee7a5c99a1a61c7160c0a3190ad65e73d
lastborn <levinachifie016@gmail.com>
2026-07-31 14:54:16 +0300

    fix: type blog categories as const tuple

--- src/lib/blog-posts.ts
+++ src/lib/blog-posts.ts
@@ -38,4 +38,4 @@ export const BLOG_POSTS: BlogPost[] = [
 ];
 
 
-export const BLOG_CATEGORIES = ["Backend", "Mobile", "Design"];
+export const BLOG_CATEGORIES = ["Backend", "Mobile", "Design"] as const;
